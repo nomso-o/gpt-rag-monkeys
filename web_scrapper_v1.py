9d277f799dccfdd7548e431be14ac30a9b7b85a5
nomso-o <nomso.oji@gmail.com>
2025-10-19 14:55:26 -0400

    changes to web_scrapper_v1.py, demo_playground.ipynb

--- web_scrapper_v1.py
+++ web_scrapper_v1.py
@@ -71,12 +71,42 @@ async def fetch_table_with_backoff_async(session, url, headers=None, columns: Op
     return _parse_table(html, columns, output_doc_name)
 
 
+# Downloading multiple Documents
 
+@backoff.on_exception(backoff.expo, (requests.exceptions.RequestException, aiohttp.ClientError), max_tries=5)
+def fetch_multiple_documents(urls : list, headers=None):
+    """
+    Fetch multiple URLs concurrently with exponential backoff on failure.
+    """
+    response = requests.get(url=urls, headers=headers, timeout=10)
+    response.raise_for_status()
+    logging.info("Successfully fetched the webpage: %s", response.url)
 
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+    else:
+        logging.error("Failed to retrieve the webpage. Status code: %s", response.status_code)
+        exit()
+    
+    # Find the document link
+    document_links = soup.findall("a", {"class": "download-link"})["href"]
 
-# if __name__ == "__main__":
-#     asyncio.run(main())
+    # Display document link to verify
+    logging.info("Document link found: %s", document_links)
 
+    # Loop through and download each document
+    for i, document_link in enumerate(document_links):
+        logging.info("Downloading document %d from %s", i + 1, document_link)       
+        document_response = requests.get(document_link, headers=headers, timeout=10)
+        document_response.raise_for_status()
 
-# if __name__ == "__main__":
-#     fetch_with_backoff(URL)
+    
+    if document_response.status_code == 200:
+        # Save each document with a unique name
+        file_name = f"report_{i + 1}.pdf"
+        with open(file_name, "wb") as file:
+            file.write(document_response.content)
+        logging.info(f"Document {i+1} downloaded successfully as report.pdf")
+    else:
+        logging.error(f"Failed to download document {i+1}. Status code:", document_response.status_code)
+
